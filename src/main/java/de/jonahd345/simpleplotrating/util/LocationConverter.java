79d46c56dd2bb0f25534e7efd6069383195d8bd6
jonahd345 <info.minejona@gmail.com>
2025-01-21 08:39:49 +0100

    feat: add LocationConverter

---
+++ src/main/java/de/jonahd345/simpleplotrating/util/LocationConverter.java
@@ -0,0 +1,35 @@
+package de.jonahd345.simpleplotrating.util;
+import com.plotsquared.core.location.Location;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+
+public class LocationConverter {
+    /**
+     * Converts a PlotSquared location to a Bukkit location using LocationFetcher.
+     *
+     * @param plotSquaredLocation the PlotSquared location to convert
+     * @return the equivalent Bukkit Location, or null if the world is not found
+     */
+    public static org.bukkit.Location toBukkitLocation(Location plotSquaredLocation) {
+        // Use LocationFetcher to get world information
+        String worldName = plotSquaredLocation.getWorldName();
+
+        // Fetch the Bukkit world
+        World bukkitWorld = Bukkit.getWorld(worldName);
+
+        if (bukkitWorld == null) {
+            return null; // Handle cases where the world is not loaded
+        }
+
+        // Create and return the Bukkit location
+        return new org.bukkit.Location(
+                bukkitWorld,
+                plotSquaredLocation.getX(),
+                plotSquaredLocation.getY(),
+                plotSquaredLocation.getZ(),
+                plotSquaredLocation.getYaw(),
+                plotSquaredLocation.getPitch()
+        );
+    }
+}
+
