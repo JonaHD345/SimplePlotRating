e2f4f8410fd8813e130c697f6d6473acc6a6a2f7
jonahd345 <info.minejona@gmail.com>
2025-01-23 10:52:11 +0100

    fix: refactor RatingCategory from enum to class

--- src/main/java/de/jonahd345/simpleplotrating/model/RatingCategory.java
+++ src/main/java/de/jonahd345/simpleplotrating/model/RatingCategory.java
@@ -4,24 +4,24 @@ import lombok.Getter;
 import org.bukkit.Material;
 
 @Getter
-public enum RatingCategory {
-    CELLAR("Keller", Material.IRON_DOOR),
+public class RatingCategory {
+    /*CELLAR("Keller", Material.IRON_DOOR),
     INTERIOR_DESIGN("Inneneinrichtung", Material.CRAFTING_TABLE),
     GARDEN("Garten", Material.DANDELION),
     EXTERIOR_VIEW("Außenansicht", Material.DANDELION),
-    CREATIVITY("Kreativität", Material.COMMAND_BLOCK, true);
+    CREATIVITY("Kreativität", Material.COMMAND_BLOCK, true);*/
 
     private final String name;
     private final Material material;
     private final boolean isLastOne;
 
-    RatingCategory(String name, Material material, boolean isLastOne) {
+    public RatingCategory(String name, Material material, boolean isLastOne) {
         this.name = name;
         this.material = material;
         this.isLastOne = isLastOne;
     }
 
-    RatingCategory(String name, Material material) {
+    public RatingCategory(String name, Material material) {
         this(name, material, false);
     }
 }
