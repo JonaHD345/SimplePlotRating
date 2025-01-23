9e6a8c3ab0d7d1710ee404ea6912a7b6f440bd31
jonahd345 <info.minejona@gmail.com>
2025-01-23 10:19:44 +0100

    feat: register PlotRateCmd in SimplePlotRating

--- src/main/java/de/jonahd345/simpleplotrating/SimplePlotRating.java
+++ src/main/java/de/jonahd345/simpleplotrating/SimplePlotRating.java
@@ -1,8 +1,10 @@
 package de.jonahd345.simpleplotrating;
 
+import de.jonahd345.simpleplotrating.command.PlotRateCommand;
 import de.jonahd345.simpleplotrating.manager.PlotManager;
 import de.jonahd345.simpleplotrating.util.Metrics;
 import lombok.Getter;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SimplePlotRating extends JavaPlugin {
@@ -13,11 +15,23 @@ public final class SimplePlotRating extends JavaPlugin {
     public void onEnable() {
        Metrics metrics = new Metrics(this, 24480);
 
-       plotManager = new PlotManager();
+       this.plotManager = new PlotManager();
+
+       this.init();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private void init() {
+        PluginManager pluginManager = getServer().getPluginManager();
+
+        getCommand("plotrate").setExecutor(new PlotRateCommand(this));
+    }
+
+    public static SimplePlotRating getInstance() {
+        return getPlugin(SimplePlotRating.class);
     }
 }
