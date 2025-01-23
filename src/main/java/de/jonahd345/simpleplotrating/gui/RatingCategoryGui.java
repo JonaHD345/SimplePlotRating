c8e35d27e214c45b398241d38cf1fe452ce3b4f4
jonahd345 <info.minejona@gmail.com>
2025-01-23 12:08:13 +0100

    feat: first version of RatingCategoryGui

--- src/main/java/de/jonahd345/simpleplotrating/gui/RatingCategoryGui.java
+++ src/main/java/de/jonahd345/simpleplotrating/gui/RatingCategoryGui.java
@@ -1,4 +1,38 @@
 package de.jonahd345.simpleplotrating.gui;
 
+import de.jonahd345.simpleplotrating.SimplePlotRating;
+import de.jonahd345.simpleplotrating.model.RatingCategory;
+import dev.triumphteam.gui.guis.Gui;
+import lombok.Getter;
+import org.bukkit.entity.Player;
+
 public class RatingCategoryGui {
+    private SimplePlotRating plugin;
+
+    private Gui gui;
+
+    @Getter
+    private int rating;
+
+    public RatingCategoryGui(SimplePlotRating plugin) {
+        this.plugin = plugin;
+    }
+
+    private void setupGui(RatingCategory ratingCategory) {
+        this.gui.updateTitle(" " + ratingCategory.getName());
+    }
+
+    public void showGui(Player player) {
+        this.gui = Gui.gui()
+                .rows(6)
+                .create();
+        this.gui.setDefaultClickAction(event -> {
+            event.setCancelled(true);
+        });
+
+        for (RatingCategory ratingCategory : this.plugin.getRatingCategories().values()) {
+            this.setupGui(ratingCategory);
+        }
+        this.gui.open(player);
+    }
 }
