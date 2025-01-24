70e7b67d4fb5681d3f99a390963012eac899e30c
jonahd345 <contact@jonahd345.de>
2025-01-24 12:36:00 +0100

    feat: all .yml services summarised in ConfigService (now everything in config.yml)

--- src/main/java/de/jonahd345/simpleplotrating/SimplePlotRating.java
+++ src/main/java/de/jonahd345/simpleplotrating/SimplePlotRating.java
@@ -3,9 +3,7 @@ package de.jonahd345.simpleplotrating;
 import de.jonahd345.simpleplotrating.command.PlotRatingCommand;
 import de.jonahd345.simpleplotrating.listener.ConnectionListener;
 import de.jonahd345.simpleplotrating.manager.PlotRatingManager;
-import de.jonahd345.simpleplotrating.service.GuiTextService;
-import de.jonahd345.simpleplotrating.service.MessageService;
-import de.jonahd345.simpleplotrating.service.SignTextService;
+import de.jonahd345.simpleplotrating.service.ConfigService;
 import de.jonahd345.simpleplotrating.util.Metrics;
 import lombok.Getter;
 import org.bukkit.plugin.PluginManager;
@@ -14,13 +12,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SimplePlotRating extends JavaPlugin {
     @Getter
-    private MessageService messageService;
-
-    @Getter
-    private GuiTextService guiTextService;
-
-    @Getter
-    private SignTextService signTextService;
+    private ConfigService configService;
 
     @Getter
     private PlotRatingManager plotRatingManager;
@@ -29,12 +21,8 @@ public final class SimplePlotRating extends JavaPlugin {
     public void onEnable() {
        Metrics metrics = new Metrics(this, 24480);
 
-       this.messageService = new MessageService(this);
-       this.messageService.loadMessages();
-       this.guiTextService = new GuiTextService(this);
-       this.guiTextService.loadTexts();
-       this.signTextService = new SignTextService(this);
-       this.signTextService.loadTexts();
+       this.configService = new ConfigService(this);
+       this.configService.loadConfig();
 
        this.plotRatingManager = new PlotRatingManager();
 
--- src/main/java/de/jonahd345/simpleplotrating/service/ConfigService.java
+++ src/main/java/de/jonahd345/simpleplotrating/service/ConfigService.java
@@ -1,4 +1,70 @@
 package de.jonahd345.simpleplotrating.service;
 
+import de.jonahd345.simpleplotrating.SimplePlotRating;
+import de.jonahd345.simpleplotrating.model.GuiText;
+import de.jonahd345.simpleplotrating.model.Message;
+import de.jonahd345.simpleplotrating.model.SignText;
+import de.jonahd345.simpleplotrating.util.FileUtil;
+import de.jonahd345.simpleplotrating.util.StringUtil;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+
 public class ConfigService {
+    private SimplePlotRating plugin;
+
+    private File file;
+
+    private FileConfiguration yamlConfiguration;
+
+    public ConfigService(SimplePlotRating plugin) {
+        this.plugin = plugin;
+        this.file = new File("plugins/" + this.plugin.getName() + "/config.yml");
+        this.yamlConfiguration = YamlConfiguration.loadConfiguration(this.file);
+    }
+
+    public void loadConfig() {
+        this.yamlConfiguration = YamlConfiguration.loadConfiguration(this.file);
+        boolean hasFileChanges = false;
+
+        //Config
+        this.yamlConfiguration.set("config.config", "Placeholder");
+        // Messages
+        for (Message message : Message.values()) {
+            if (!(this.file.exists()) || this.yamlConfiguration.getString("messages." + message.name().toLowerCase()) == null) {
+                this.yamlConfiguration.set("messages." + message.name().toLowerCase(), message.getDefaultMessage());
+                hasFileChanges = true;
+                // set Message's message to his default message and skip the next line, because by new mess yamlConfiguration.getString is null
+                message.setMessage(StringUtil.translateColorCodes(message.getDefaultMessage()));
+                continue;
+            }
+            message.setMessage(StringUtil.translateColorCodes(this.yamlConfiguration.getString("messages." + message.name().toLowerCase())));
+        }
+        // Text for Guis
+        for (GuiText text : GuiText.values()) {
+            if (!(this.file.exists()) || this.yamlConfiguration.getString("guitexts." + text.name().toLowerCase()) == null) {
+                this.yamlConfiguration.set("guitexts." + text.name().toLowerCase(), text.getDefaultText());
+                hasFileChanges = true;
+                // set Text's text to his default text and skip the next line, because by new mess yamlConfiguration.getString is null
+                text.setText(StringUtil.translateColorCodes(text.getDefaultText()));
+                continue;
+            }
+            text.setText(StringUtil.translateColorCodes(this.yamlConfiguration.getString("guitexts." + text.name().toLowerCase())));
+        }
+        // Texts for signs
+        for (SignText text : SignText.values()) {
+            if (!(this.file.exists()) || this.yamlConfiguration.getString("signtexts." + text.name().toLowerCase()) == null) {
+                this.yamlConfiguration.set("signtexts." + text.name().toLowerCase(), text.getDefaultText());
+                hasFileChanges = true;
+                // set Text's text to his default text and skip the next line, because by new mess yamlConfiguration.getString is null
+                text.setText(StringUtil.translateColorCodes(text.getDefaultText()));
+                continue;
+            }
+            text.setText(StringUtil.translateColorCodes(this.yamlConfiguration.getString("signtexts." + text.name().toLowerCase())));
+        }
+        if (hasFileChanges) {
+            FileUtil.saveFile(this.yamlConfiguration, this.file);
+        }
+    }
 }
