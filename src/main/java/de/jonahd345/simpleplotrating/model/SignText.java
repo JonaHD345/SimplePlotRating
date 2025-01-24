a687fa00c4162ff5948934d61acbb1cc985672b3
jonahd345 <contact@jonahd345.de>
2025-01-24 11:18:22 +0100

    feat: edit default text in SignText

--- src/main/java/de/jonahd345/simpleplotrating/model/SignText.java
+++ src/main/java/de/jonahd345/simpleplotrating/model/SignText.java
@@ -6,9 +6,9 @@ import lombok.Setter;
 @Getter
 public enum SignText {
     // v1.0
-    RATING_LINE_1("&8Rated by"),
+    RATING_LINE_1("&0Rated by"),
     RATING_LINE_2("&a%rated_player%"),
-    RATING_LINE_3("&8on %date%"),
+    RATING_LINE_3("&0on %date%"),
     RATING_LINE_4("&a%rating%/25 points");
 
     private final String defaultText;
